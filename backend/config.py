c4f3c15ddca1a9d9ecd0d18fcd5e807bf1515b68
Om <ommakhija2004@gmail.com>
2026-01-18 15:57:15 +0530

    final v1 to main

--- backend/config.py
+++ backend/config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = "https://lsodktpfctcewhbhowzo.supabase.co"
+SUPABASE_KEY = "sb_secret_dXk3VUPBQLr3V2NRGCC7vQ_pBcl4JjE"
