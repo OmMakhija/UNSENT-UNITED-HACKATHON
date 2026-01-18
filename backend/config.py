57a54ba52bb602b919803db4d0d0ea37e2c705e2
Om <ommakhija2004@gmail.com>
2026-01-18 16:08:09 +0530

    final v1 to main

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,8 @@
+# backend/config.py
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+SUPABASE_URL = os.environ.get("https://lsodktpfctcewhbhowzo.supabase.co")
+SUPABASE_KEY = os.environ.get("sb_secret_dXk3VUPBQLr3V2NRGCC7vQ_pBcl4JjE")
 
-SUPABASE_URL = "https://lsodktpfctcewhbhowzo.supabase.co"
-SUPABASE_KEY = sb_secret_dXk3VUPBQLr3V2NRGCC7vQ_pBcl4JjE
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise RuntimeError("Supabase environment variables not set")
