796e9b8ba614add7ed2114f0d37fb765e7292ffe
Om <ommakhija2004@gmail.com>
2026-01-18 16:00:24 +0530

    final v1 to main

--- backend/config.py
+++ backend/config.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SUPABASE_URL = "https://lsodktpfctcewhbhowzo.supabase.co"
-SUPABASE_KEY = "sb_secret_dXk3VUPBQLr3V2NRGCC7vQ_pBcl4JjE"
+SUPABASE_KEY = sb_secret_dXk3VUPBQLr3V2NRGCC7vQ_pBcl4JjE
